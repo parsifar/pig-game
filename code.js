95134c8c718c0e7f5fa742b02c4cbb9fc82e4846
Ali Parsifar <parsifar@gmail.com>
2020-01-22 11:35:17 -0500

    added sound effects and some settings

--- code.js
+++ code.js
@@ -5,6 +5,9 @@ let activePlayer
 let newGameBtn = document.getElementById("new-game")
 let rollBtn = document.getElementById("roll")
 let holdBtn = document.getElementById("hold")
+let winningScore = 100
+let doubleSixRule = false
+let lastDice
 
 init()
 //2-roll button function: a:random dice function b:updates dice img c: checks if its 1 or not if it is then change active player and sets current to 0 if not d:adds to active player current score
@@ -13,6 +16,7 @@ function rollDice() {
 }
 
 function changeActive() {
+    lastDice = undefined
     if (activePlayer === 0) {
         activePlayer = 1
         document.querySelector(".box-0").classList.remove("active")
@@ -33,20 +37,32 @@ rollBtn.addEventListener("click", function () {
         let dice = rollDice()
         document.getElementById("dice-img").src = 'images/' + dice + '_dots.png'
         if (dice === 1) {
+            document.getElementById("ah").play()
             currentScore = 0
             updateCurrentScore()
             changeActive()
             setTimeout(function () { document.getElementById("dice-img").src = "" }, 1000)
+        } else if (doubleSixRule && dice === 6 && lastDice === 6) {
+            document.getElementById("bye").play()
+            currentScore = 0
+            updateCurrentScore()
+            scores[activePlayer] = 0
+            document.querySelector(".score-" + activePlayer).textContent = 0
+            changeActive()
+
         } else {
             currentScore += dice
             updateCurrentScore()
+            lastDice = dice
         }
     }, 500)
 })
 //3 hold btn function: a:checks if winner b:ads the current to active player score c:sets current to 0 d:changes the active player
 function checkWin() {
-    if (scores[activePlayer] >= 100) {
-        alert("player " + activePlayer + " wins")
+    if (scores[activePlayer] >= winningScore) {
+        document.getElementById("applause").play()
+        alert("player " + (activePlayer + 1) + " wins")
+        init()
     }
 }
 holdBtn.addEventListener("click", function () {
@@ -73,3 +89,12 @@ function init() {
 }
 
 newGameBtn.addEventListener("click", init)
+
+//5 - checking settings
+function checkSettings() {
+    doubleSixRule = document.getElementById("dblsix").checked
+    winningScore = document.getElementById("win-score").value
+}
+
+document.getElementById("dblsix").addEventListener("change", checkSettings)
+document.getElementById("win-score").addEventListener("change", checkSettings)
